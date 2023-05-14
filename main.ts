46ee8490bda70ecf0b2e4d701053a26dff02a9bb
mermoz68 <47685555+mermoz68@users.noreply.github.com>
2023-05-14 18:35:45 +0200

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,27 +1,23 @@
-let strip = neopixel.create(DigitalPin.P13, 4, NeoPixelMode.RGB)
+function base () {
+    strip.setPixelColor(0, neopixel.colors(NeoPixelColors.Blue))
+    strip.setPixelColor(1, neopixel.colors(NeoPixelColors.Red))
+    strip.setPixelColor(2, neopixel.colors(NeoPixelColors.Green))
+    strip.setPixelColor(3, neopixel.colors(NeoPixelColors.White))
+    strip.show()
+}
+let strip: neopixel.Strip = null
+strip = neopixel.create(DigitalPin.P13, 4, NeoPixelMode.RGB)
 strip.setBrightness(5)
-strip.setPixelColor(0, neopixel.colors(NeoPixelColors.Blue))
-strip.setPixelColor(1, neopixel.colors(NeoPixelColors.Red))
-strip.setPixelColor(2, neopixel.colors(NeoPixelColors.Green))
-strip.setPixelColor(3, neopixel.colors(NeoPixelColors.White))
-strip.show()
+base()
 basic.forever(function () {
     for (let index = 0; index <= 25; index++) {
         strip.setBrightness(10 * index)
-        strip.setPixelColor(0, neopixel.colors(NeoPixelColors.Blue))
-        strip.setPixelColor(1, neopixel.colors(NeoPixelColors.Red))
-        strip.setPixelColor(2, neopixel.colors(NeoPixelColors.Green))
-        strip.setPixelColor(3, neopixel.colors(NeoPixelColors.White))
-        strip.show()
+        base()
         basic.pause(20)
     }
     for (let index = 0; index <= 25; index++) {
         strip.setBrightness(250 - 10 * index)
-        strip.setPixelColor(0, neopixel.colors(NeoPixelColors.Blue))
-        strip.setPixelColor(1, neopixel.colors(NeoPixelColors.Red))
-        strip.setPixelColor(2, neopixel.colors(NeoPixelColors.Green))
-        strip.setPixelColor(3, neopixel.colors(NeoPixelColors.White))
-        strip.show()
+        base()
         basic.pause(20)
     }
 })
