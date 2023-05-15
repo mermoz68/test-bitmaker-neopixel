6651049632c1c9d14346dbd4e46a46119fa55030
mermoz68 <47685555+mermoz68@users.noreply.github.com>
2023-05-15 10:55:32 +0200

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,7 +3,6 @@ function base () {
     strip.setPixelColor(1, neopixel.colors(NeoPixelColors.Red))
     strip.setPixelColor(2, neopixel.colors(NeoPixelColors.Green))
     strip.setPixelColor(3, neopixel.colors(NeoPixelColors.White))
-    strip.show()
 }
 let strip: neopixel.Strip = null
 strip = neopixel.create(DigitalPin.P13, 4, NeoPixelMode.RGB)
@@ -13,16 +12,20 @@ basic.forever(function () {
     for (let index = 0; index <= 25; index++) {
         strip.setBrightness(10 * index)
         base()
-        basic.pause(20)
+        strip.show()
+        basic.pause(100)
     }
     for (let index = 0; index <= 25; index++) {
         strip.setBrightness(250 - 10 * index)
         base()
-        basic.pause(20)
+        strip.show()
+        basic.pause(100)
     }
     for (let index = 0; index <= 4; index++) {
+        strip.setBrightness(255)
         strip.shift(1)
         base()
+        strip.show()
         basic.pause(1000)
     }
 })
